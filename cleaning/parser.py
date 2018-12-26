055f8f8dc2a7d71fabf083be2602124756d29b23
Derek Willis <dwillis@gmail.com>
2018-12-25 21:39:08 -0500

    updated parser for 2018 general

--- cleaning/parser.py
+++ cleaning/parser.py
@@ -4,7 +4,7 @@ import csv
 
 results = []
 
-r = requests.get("https://sos-tn-gov-files.tnsosfiles.com/180802_ResultsbyPrecinct.xlsx")
+r = requests.get("https://sos-tn-gov-files.tnsosfiles.com/Nov%202018%20General%20results.xlsx")
 workbook = xlrd.open_workbook(file_contents=r.content)
 ws = workbook.sheets()[0]
 
@@ -12,19 +12,19 @@ for r in range(1, ws.nrows):
     row = ws.row_values(r)
     county = row[0]
     precinct = row[2]
-    if 'Tennessee House of Representatives' in row[6]:
+    if 'Tennessee House of Representatives' in row[7]:
         office = 'State Representative'
-        district = row[6].split(' District ')[1]
-    elif 'Tennessee Senate' in row[6]:
+        district = row[7].split(' District ')[1]
+    elif 'Tennessee Senate' in row[7]:
         office = 'State Senate'
-        district = row[6].split(' District ')[1]
-    elif 'United States House of Representatives' in row[6]:
+        district = row[7].split(' District ')[1]
+    elif 'United States House of Representatives' in row[7]:
         office = 'U.S. House'
-        district = row[6].split(' District ')[1]
+        district = row[7].split(' District ')[1]
     else:
-        office = row[6]
+        office = row[7]
         district = None
-    cand_votes = row[10:]
+    cand_votes = row[11:]
     cands_with_votes = zip(*[cand_votes[i::4] for i in range(4)])
     for cand, party, votes, fill in cands_with_votes:
         if votes != '':
@@ -35,7 +35,7 @@ for r in range(1, ws.nrows):
             candidate = cand
             results.append([county, precinct, office, district, party, candidate, votes])
 
-with open("2018/20180802__tn__primary__precinct.csv", 'w') as csvfile:
+with open("2018/20181106__tn__general__precinct.csv", 'w') as csvfile:
     csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_NONNUMERIC)
     csvwriter.writerow(['county','precinct','office','district','party','candidate','votes'])
     csvwriter.writerows(results)
