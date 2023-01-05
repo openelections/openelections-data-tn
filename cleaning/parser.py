5468234c15b5586b01d41b1c1ce11ce07589c0b3
Derek Willis <dwillis@gmail.com>
2023-01-05 13:03:37 -0500

    2022 update:

--- cleaning/parser.py
+++ cleaning/parser.py
@@ -4,7 +4,7 @@ import csv
 
 results = []
 
-r = requests.get("https://sos-tn-gov-files.tnsosfiles.com/Nov2020PrecinctDetail.xlsx")
+r = requests.get("https://sos-prod.tnsosgovfiles.com/s3fs-public/document/20221108ByPrecinct.xlsx")
 workbook = xlrd.open_workbook(file_contents=r.content)
 ws = workbook.sheets()[0]
 
@@ -12,17 +12,17 @@ for r in range(1, ws.nrows):
     row = ws.row_values(r)
     county = row[0]
     precinct = row[2]
-    if 'Tennessee House of Representatives' in row[6]:
+    if 'Tennessee House of Representatives' in row[7]:
         office = 'State Representative'
-        district = row[6].split(' District ')[1]
-    elif 'Tennessee Senate' in row[6]:
+        district = row[7].split(' District ')[1]
+    elif 'Tennessee Senate' in row[7]:
         office = 'State Senate'
-        district = row[6].split(' District ')[1]
-    elif 'United States House of Representatives' in row[6]:
+        district = row[7].split(' District ')[1]
+    elif 'United States House of Representatives' in row[7]:
         office = 'U.S. House'
-        district = row[6].split(' District ')[1]
+        district = row[7].split(' District ')[1]
     else:
-        office = row[6]
+        office = row[7]
         district = None
     cand_votes = row[9:]
     cands_with_votes = zip(*[cand_votes[i::4] for i in range(4)])
@@ -35,7 +35,7 @@ for r in range(1, ws.nrows):
             candidate = cand
             results.append([county, precinct, office, district, party, candidate, votes])
 
-with open("2020/20201103__tn__general__precinct.csv", 'w') as csvfile:
+with open("2022/20221108__tn__general__precinct.csv", 'w') as csvfile:
     csvwriter = csv.writer(csvfile)
     csvwriter.writerow(['county','precinct','office','district','party','candidate','votes'])
     csvwriter.writerows(results)
